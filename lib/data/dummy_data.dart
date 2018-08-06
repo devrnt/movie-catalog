ae00eb99268b7b2274dd1a0d322fbbe617db761d
devrnt <devrient.jonas@gmail.com>
2018-08-06 22:02:16 +0200

    Add dummy data

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,131 @@
+import 'package:movie_catalog/models/movie.dart';
+import 'package:movie_catalog/models/torrent.dart';
+
+List<Movie> dummyData = [
+  new Movie(
+      8521,
+      'Pina',
+      2011,
+      'In modern dance since the 1970s, few choreographers have had more influence in the medium than the late Pina Bausch. This film explores the life and work of this artist of movement while we see her company perform her most notable creations where basic things like water, dirt and even gravity take on otherworldly qualities in their dancing.',
+      ['Documentary'],
+      7.7,
+      'https://yts.am/movie/pina-2011',
+      'tt1440266',
+      'https://yts.am/assets/images/movies/pina_2011/background.jpg',
+      'https://yts.am/assets/images/movies/pina_2011/large-cover.jpg',
+      [
+        new Torrent(
+            'https://yts.am/assets/images/movies/pina_2011/large-cover.jpg',
+            '90CBAC6473DD54F9911E4ADBE0C0CE7C0A2DE6FC',
+            '720p',
+            931649290,
+            new DateTime.now(),
+            74,
+            57)
+      ]),
+  new Movie(
+      8521,
+      'Pina',
+      2011,
+      'In modern dance since the 1970s, few choreographers have had more influence in the medium than the late Pina Bausch. This film explores the life and work of this artist of movement while we see her company perform her most notable creations where basic things like water, dirt and even gravity take on otherworldly qualities in their dancing.',
+      ['Documentary'],
+      7.7,
+      'https://yts.am/movie/pina-2011',
+      'tt1440266',
+      'https://yts.am/assets/images/movies/pina_2011/background.jpg',
+      'https://yts.am/assets/images/movies/pina_2011/large-cover.jpg',
+      [
+        new Torrent(
+            'https://yts.am/assets/images/movies/pina_2011/large-cover.jpg',
+            '90CBAC6473DD54F9911E4ADBE0C0CE7C0A2DE6FC',
+            '720p',
+            931649290,
+            new DateTime.now(),
+            74,
+            57)
+      ]),
+  new Movie(
+      8521,
+      'Pina',
+      2011,
+      'In modern dance since the 1970s, few choreographers have had more influence in the medium than the late Pina Bausch. This film explores the life and work of this artist of movement while we see her company perform her most notable creations where basic things like water, dirt and even gravity take on otherworldly qualities in their dancing.',
+      ['Documentary'],
+      7.7,
+      'https://yts.am/movie/pina-2011',
+      'tt1440266',
+      'https://yts.am/assets/images/movies/pina_2011/background.jpg',
+      'https://yts.am/assets/images/movies/pina_2011/large-cover.jpg',
+      [
+        new Torrent(
+            'https://yts.am/assets/images/movies/pina_2011/large-cover.jpg',
+            '90CBAC6473DD54F9911E4ADBE0C0CE7C0A2DE6FC',
+            '720p',
+            931649290,
+            new DateTime.now(),
+            74,
+            57)
+      ]),
+  new Movie(
+      8521,
+      'Pina',
+      2011,
+      'In modern dance since the 1970s, few choreographers have had more influence in the medium than the late Pina Bausch. This film explores the life and work of this artist of movement while we see her company perform her most notable creations where basic things like water, dirt and even gravity take on otherworldly qualities in their dancing.',
+      ['Documentary'],
+      7.7,
+      'https://yts.am/movie/pina-2011',
+      'tt1440266',
+      'https://yts.am/assets/images/movies/pina_2011/background.jpg',
+      'https://yts.am/assets/images/movies/pina_2011/large-cover.jpg',
+      [
+        new Torrent(
+            'https://yts.am/assets/images/movies/pina_2011/large-cover.jpg',
+            '90CBAC6473DD54F9911E4ADBE0C0CE7C0A2DE6FC',
+            '720p',
+            931649290,
+            new DateTime.now(),
+            74,
+            57)
+      ]),
+  new Movie(
+      8521,
+      'Pina',
+      2011,
+      'In modern dance since the 1970s, few choreographers have had more influence in the medium than the late Pina Bausch. This film explores the life and work of this artist of movement while we see her company perform her most notable creations where basic things like water, dirt and even gravity take on otherworldly qualities in their dancing.',
+      ['Documentary'],
+      7.7,
+      'https://yts.am/movie/pina-2011',
+      'tt1440266',
+      'https://yts.am/assets/images/movies/pina_2011/background.jpg',
+      'https://yts.am/assets/images/movies/pina_2011/large-cover.jpg',
+      [
+        new Torrent(
+            'https://yts.am/assets/images/movies/pina_2011/large-cover.jpg',
+            '90CBAC6473DD54F9911E4ADBE0C0CE7C0A2DE6FC',
+            '720p',
+            931649290,
+            new DateTime.now(),
+            74,
+            57)
+      ]),
+  new Movie(
+      8521,
+      'Pina',
+      2011,
+      'In modern dance since the 1970s, few choreographers have had more influence in the medium than the late Pina Bausch. This film explores the life and work of this artist of movement while we see her company perform her most notable creations where basic things like water, dirt and even gravity take on otherworldly qualities in their dancing.',
+      ['Documentary'],
+      7.7,
+      'https://yts.am/movie/pina-2011',
+      'tt1440266',
+      'https://yts.am/assets/images/movies/pina_2011/background.jpg',
+      'https://yts.am/assets/images/movies/pina_2011/large-cover.jpg',
+      [
+        new Torrent(
+            'https://yts.am/assets/images/movies/pina_2011/large-cover.jpg',
+            '90CBAC6473DD54F9911E4ADBE0C0CE7C0A2DE6FC',
+            '720p',
+            931649290,
+            new DateTime.now(),
+            74,
+            57)
+      ])
+];
