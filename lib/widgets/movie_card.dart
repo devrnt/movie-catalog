af34a8e19d8c1a2a09b472115c8898cd0f2e7370
devrnt <devrient.jonas@gmail.com>
2018-08-09 16:04:50 +0200

    Add navigation form home to details

--- lib/widgets/movie_card.dart
+++ lib/widgets/movie_card.dart
@@ -1,49 +1,66 @@
 import 'package:flutter/material.dart';
 
+import 'package:movie_catalog/screens/movie_details_screen.dart';
+import 'package:movie_catalog/services/movie_service.dart';
+
 class MovieCard extends StatelessWidget {
+  final int id;
   final String title;
   final int year;
   final String imageUrl;
 
-  MovieCard({this.title, this.year, this.imageUrl});
+  final MovieService _movieService = new MovieService();
+
+  MovieCard({this.id, this.title, this.year, this.imageUrl});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 1.2,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Center(
-            child: Image.network(
-              imageUrl,
-              fit: BoxFit.cover,
-            ),
-          ),
-          Expanded(
-              child: Container(
-            padding: EdgeInsets.only(left: 7.0),
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 2.0),
-                    child: Text(
-                      title,
-                    ),
+    return GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => MovieDetails(
+                    movie: _movieService.getMovie(id),
                   ),
-                  Text(
-                    year.toString(),
-                    style: TextStyle(
-                      color: Colors.grey,
-                    ),
-                  )
-                ]),
-          )),
-        ],
-      ),
-    );
+            ),
+          );
+        },
+        child: Card(
+          elevation: 1.2,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Center(
+                child: Image.network(
+                  imageUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Expanded(
+                  child: Container(
+                padding: EdgeInsets.only(left: 7.0),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 2.0),
+                        child: Text(
+                          title,
+                        ),
+                      ),
+                      Text(
+                        year.toString(),
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      )
+                    ]),
+              )),
+            ],
+          ),
+        ));
   }
 }
