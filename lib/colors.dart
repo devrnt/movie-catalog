9b4bb982e810df93f667a7b3b6a9a89584197903
devrnt <devrient.jonas@gmail.com>
2018-08-09 18:33:51 +0200

    Change theming Add colors Add fonts Update pubspec Change main

---
+++ lib/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF212121);
+const kPrimaryLight = Color(0xFF484848);
+const kPrimaryDark =  Color(0x60000000);
+
+const kSecondaryColor = Color(0xFFFFFFFF);
+
+const kAccentColor = Color(0xFF316d13);
+
+const kIconColor = Color(0xFF9E9E9E);
