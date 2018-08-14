a07e04fa5baa1af8e1a37ae38e82f4674a5a85e4
devrnt <devrient.jonas@gmail.com>
2018-08-14 17:03:46 +0200

    Add movie service to fetch the movies

--- lib/services/movie_service.dart
+++ lib/services/movie_service.dart
@@ -1,10 +1,39 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:flutter/foundation.dart';
+
 import 'package:movie_catalog/data/dummy_data.dart';
 import 'package:movie_catalog/models/movie.dart';
+
+const url = 'https://yts.am/api/v2/list_movies.json';
 
 class MovieService {
   List<Movie> listOfMovies;
   MovieService() {
     listOfMovies = movies;
+  }
+
+  // Fetch all the movies
+  // Keep the order of the API
+  Future<List<Movie>> fetchAllMovies(http.Client client) async {
+    final response = await http.get(url);
+    print(response.body);
+    if (response.statusCode == 200) {
+      print(response.body);
+      // Use the compute function to run parsePhotos in a separate isolate
+      return compute(parseMovies, response.body);
+    } else {
+      throw Exception(
+          'Failed to load movies: Check if the api ${url} is still online. If not the case check if the mapping is still correct.');
+    }
+  }
+
+  // Convert the list of movies from API to list
+  List<Movie> parseMovies(String responseBody) {
+    final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
+    return parsed.map<Movie>((json) => Movie.fromJson(json)).toList();
   }
 
   Movie getMovie(int id) {
