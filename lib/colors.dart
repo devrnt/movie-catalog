d2b15d80185b239eedcc682895fa1c173ba3d53c
devrnt <devrient.jonas@gmail.com>
2018-08-18 19:05:58 +0200

    Add method to get popular movies in movie service

--- lib/colors.dart
+++ lib/colors.dart
@@ -6,6 +6,6 @@ const kPrimaryDark =  Color(0x60000000);
 
 const kSecondaryColor = Color(0xFFFFFFFF);
 
-const kAccentColor = Color(0xFF319d13);
+const kAccentColor = Color(0xFF318d13);
 
 const kIconColor = Color(0xFF9E9E9E);
