bc627a19a97be789d9fb91b01737a117e737cc44
devrnt <devrient.jonas@gmail.com>
2018-08-18 18:22:19 +0200

    Add check for internet connection

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,14 +1,20 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:flutter/services.dart';
+
 import 'package:movie_catalog/services/movie_service.dart';
 
 import 'package:movie_catalog/widgets/movie_card.dart';
 
 import 'package:movie_catalog/models/movie.dart';
 
-import 'package:http/http.dart' as http;
 import 'package:movie_catalog/widgets/movie_grid.dart';
+
+import 'package:connectivity/connectivity.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -19,16 +25,47 @@ class _HomeScreenState extends State<HomeScreen> {
   ScrollController _scrollController;
   MovieService _movieService;
 
+  String _connectionStatus = 'Unknown';
+  final Connectivity _connectivity = new Connectivity();
+  StreamSubscription<ConnectivityResult> _connectivitySubscription;
+
   @override
   void initState() {
     _scrollController = new ScrollController();
     _movieService = new MovieService();
     super.initState();
+    initConnectivity();
+    _connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen((ConnectivityResult result) {
+      setState(() => _connectionStatus = result.toString());
+    });
   }
 
   @override
   void dispose() {
+    _connectivitySubscription.cancel();
     super.dispose();
+  }
+
+  Future<Null> initConnectivity() async {
+    String connectionStatus;
+    try {
+      connectionStatus = (await _connectivity.checkConnectivity()).toString();
+    } on PlatformException catch (e) {
+      print('There occured an error: ' + e.toString());
+      connectionStatus = 'Failed to get connectivity';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) {
+      return;
+    }
+
+    setState(() {
+      _connectionStatus = connectionStatus;
+    });
   }
 
   @override
@@ -48,15 +85,17 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
-      body: FutureBuilder<List<Movie>>(
-        future: _movieService.fetchLatestMovies(http.Client(), 1),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) print(snapshot.error);
-          return snapshot.hasData
-              ? MovieGrid(movies: snapshot.data)
-              : Center(child: CircularProgressIndicator());
-        },
-      ),
+      body: !online()
+          ? showAlert()
+          : FutureBuilder<List<Movie>>(
+              future: _movieService.fetchLatestMovies(http.Client(), 1),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) print(snapshot.error);
+                return snapshot.hasData
+                    ? MovieGrid(movies: snapshot.data)
+                    : Center(child: CircularProgressIndicator());
+              },
+            ),
       // body: GridView.count(
       //   primary: true,
       //   padding: EdgeInsets.fromLTRB(2.0, 3.0, 2.0, 3.0),
@@ -67,33 +106,42 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
+  Widget showAlert() {
+    print('Alert was shown');
+    _neverSatisfied();
+    return null;
+  }
 
+  bool online() {
+    return _connectionStatus == 'ConnectivityResult.none' ? false : true;
+  }
 
-  Widget _buildGridTiles(List<Movie> movies) {
-    print(movies.length);
-    return GridView.builder(
-      padding: EdgeInsets.fromLTRB(2.0, 3.0, 2.0, 3.0),
-      controller: _scrollController,
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3,
-        childAspectRatio: 0.555,
-      ),
-      itemCount: movies.length,
-      itemBuilder: (context, index) {
-        return MovieCard(
-          movie: movies[index],
+  Future<Null> _neverSatisfied() async {
+    return showDialog<Null>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('No internet connection'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('Please turn on your internet.'),
+                Text(
+                  'This app will be closed.',
+                  style: TextStyle(color: Colors.grey, fontSize: 14.0),
+                )
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('EXIT'),
+              onPressed: () => SystemNavigator.pop(),
+            ),
+          ],
         );
       },
     );
-
-    // Use dummt data
-    // List<MovieCard> movieCards = new List<MovieCard>();
-
-    // movies.forEach((movie) {
-    //   movieCards.add(MovieCard(
-    //     id: movie.id,
-    //   ));
-    // });
-    // return movieCards;
   }
 }
