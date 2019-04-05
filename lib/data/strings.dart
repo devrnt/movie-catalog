a7792cfd163a3fcfa4d2d9fdb03b152eddecc61e
devrnt <devrient.jonas@gmail.com>
2019-04-05 20:01:10 +0200

    Add strings file with all string for use in the app

---
+++ lib/data/strings.dart
@@ -0,0 +1,8 @@
+class Strings {
+  static String appName = 'Movie Catalog';
+
+
+  static String tabLatest = 'Latest';
+  static String tabTopRated = 'Top rated';
+  static String tabLibrary = 'Liked';
+}
