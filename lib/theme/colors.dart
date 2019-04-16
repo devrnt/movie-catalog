3640c44d15d29b6ee2acdcff4f42bcaa932870cf
devrnt <devrient.jonas@gmail.com>
2019-04-16 11:28:39 +0200

    Move colors and theme config to own folder

---
+++ lib/theme/colors.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+// DARK
+
+class BlackColor {
+  // background scaffold
+  static const kPrimaryColor = Color(0xFF1E1E24);
+
+// movie card background color
+  static const kPrimaryLight = Color(0xFF292B33);
+
+// atm nothing
+  static const kPrimaryDark = Color(0xFF0A090F);
+
+  static const kSecondaryColor = Color(0xFFFFFFFF);
+
+  static const kAccentColor = Color(0xFFFFCC09);
+
+  static const kIconColor = Color(0xFF72727B);
+}
+
+class LightColor {
+  static const kPrimaryColor = Color(0xFFF1F1F1);
+
+// movie card background color
+  static const kPrimaryLight = Color(0xFFFFFFFF);
+
+// atm nothing
+  static const kPrimaryDark = Color(0xFF2B2A2A);
+
+  static const kSecondaryColor = Color(0xFF000000);
+
+  static const kAccentColor = Color(0xFFFFCC09);
+
+  static const kIconColor = Color(0xFF72727B);
+}
---
+++ lib/theme/theme_builder.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:movie_catalog/theme/colors.dart';
+
+class ThemeBuilder {
+  static ThemeData buildBlackTheme() {
+    final ThemeData base = ThemeData.dark();
+    return base.copyWith(
+      accentColor: BlackColor.kAccentColor,
+      primaryColor: BlackColor.kPrimaryColor,
+      primaryColorLight: BlackColor.kPrimaryLight,
+      primaryColorDark: BlackColor.kPrimaryDark,
+      scaffoldBackgroundColor: BlackColor.kPrimaryColor,
+      cardColor: BlackColor.kPrimaryDark,
+      textSelectionColor: BlackColor.kAccentColor,
+      textTheme: _buildBlackTextTheme(base.textTheme),
+      primaryTextTheme: _buildBlackTextTheme(base.primaryTextTheme),
+      accentTextTheme: _buildBlackTextTheme(base.accentTextTheme),
+      primaryIconTheme: base.iconTheme.copyWith(color: BlackColor.kIconColor),
+      canvasColor: BlackColor.kPrimaryLight,
+    );
+  }
+
+  static ThemeData buildLightTheme() {
+    final ThemeData base = ThemeData.light();
+    return base.copyWith(
+      accentColor: LightColor.kAccentColor,
+      primaryColor: LightColor.kPrimaryColor,
+      primaryColorLight: LightColor.kPrimaryLight,
+      primaryColorDark: LightColor.kPrimaryDark,
+      appBarTheme: base.appBarTheme
+          .copyWith(color: LightColor.kPrimaryLight.withOpacity(0.95)),
+      scaffoldBackgroundColor: LightColor.kPrimaryColor,
+      cardColor: LightColor.kPrimaryDark,
+      textSelectionColor: LightColor.kAccentColor,
+      textTheme: _buildLightTextTheme(base.textTheme),
+      primaryTextTheme: _buildLightTextTheme(base.primaryTextTheme),
+      accentTextTheme: _buildLightTextTheme(base.accentTextTheme),
+      primaryIconTheme: base.iconTheme.copyWith(color: LightColor.kIconColor),
+      canvasColor: LightColor.kPrimaryLight,
+    );
+  }
+
+  static TextTheme _buildBlackTextTheme(TextTheme base) {
+    return base
+        .copyWith(
+          headline: base.headline.copyWith(
+            fontWeight: FontWeight.normal,
+          ),
+          title: base.title.copyWith(fontSize: 18.0),
+          caption: base.caption.copyWith(
+            fontWeight: FontWeight.normal,
+            fontSize: 14.0,
+          ),
+        )
+        .apply(
+          displayColor: BlackColor.kSecondaryColor,
+          bodyColor: BlackColor.kSecondaryColor,
+        );
+  }
+
+  static TextTheme _buildLightTextTheme(TextTheme base) {
+    return base
+        .copyWith(
+          headline: base.headline.copyWith(
+              fontWeight: FontWeight.normal, color: LightColor.kPrimaryDark),
+          title: base.title.copyWith(fontSize: 18.0),
+          caption: base.caption.copyWith(
+            fontWeight: FontWeight.normal,
+            fontSize: 14.0,
+          ),
+        )
+        .apply(
+          displayColor: LightColor.kPrimaryColor,
+          bodyColor: LightColor.kSecondaryColor,
+        );
+  }
+}
