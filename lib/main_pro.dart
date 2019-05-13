9d8a661c1830050872232362c58a1bdeb6262db3
devrnt <devrient.jonas@gmail.com>
2019-05-13 18:42:03 +0200

    Add error reporting in main files

--- lib/main_pro.dart
+++ lib/main_pro.dart
@@ -1,20 +1,42 @@
+import 'dart:async';
+
+import 'package:debug_mode/debug_mode.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_catalog/bloc/bloc_provider.dart';
 import 'package:movie_catalog/bloc/theme_bloc.dart';
 import 'package:movie_catalog/config/flavor_config.dart';
+import 'package:movie_catalog/config/keys.dart';
+import 'package:movie_catalog/services/sentry_service.dart';
+import 'package:sentry/sentry.dart';
 
 import 'main.dart';
 
-void main() {
-  final flavorConfig = FlavorConfig(
-    child: MovieCatalog(),
-    flavorBuild: FlavorBuild.Pro,
-  );
+final SentryClient _sentryClient = new SentryClient(dsn: Keys.sentryDsn);
 
-  runApp(
-    BlocProvider<ThemeBloc>(
-      bloc: ThemeBloc(),
-      child: flavorConfig,
-    ),
-  );
+Future<Null> main() async {
+  FlutterError.onError = (FlutterErrorDetails details) async {
+    if (DebugMode.isInDebugMode) {
+      // In development mode simply print to console.
+      FlutterError.dumpErrorToConsole(details);
+    } else {
+      // In production mode report to the application zone to report to Sentry
+      Zone.current.handleUncaughtError(details.exception, details.stack);
+    }
+  };
+
+  runZoned<Future<Null>>(() async {
+    final flavorConfig = FlavorConfig(
+      child: MovieCatalog(),
+      flavorBuild: FlavorBuild.Pro,
+    );
+
+    runApp(
+      BlocProvider<ThemeBloc>(
+        bloc: ThemeBloc(),
+        child: flavorConfig,
+      ),
+    );
+  }, onError: (error, stackTrace) async {
+    await SentryService.reportError(_sentryClient, error, stackTrace);
+  });
 }
